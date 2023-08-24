37351dc65471faf7de330ae95f9eedcf19d79fef
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2023-08-24 17:09:02 +0700

    Update medeka.js

--- js/medeka.js
+++ js/medeka.js
@@ -21,7 +21,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+    document.getElementById("header").style.top = "10px";
   } else {
     document.getElementById("header").style.top = "-100px";
   }
